18386838b81ca48d56bbe5b48f38413e93313cae
Griffin Johnson <gjohnson@supplyframe.com>
2022-08-23 16:14:06 -0700

    update dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,12 @@
 set fish_greeting
 set -g fish_key_bindings fish_user_key_bindings
 # starship init command
-starship init fish | source
+# starship init fish | source
 
-load_nvm
+# load_nvm
 set PATH ~/.pyenv/versions/2.7.18/bin $PATH
+
+# Bun
+# set -Ux BUN_INSTALL "/Users/griffinjohnson/.bun"
+# set -px --path PATH "/Users/griffinjohnson/.bun/bin"
+
