380a9a283361b423682cf69ca14470325728736c
Griffin Johnson <gjohnson@supplyframe.com>
2023-05-15 20:17:15 -0700

    add config

--- fish/functions/load_nvm.fish
+++
@@ -1,5 +0,0 @@
-function load_nvm --on-variable="PWD"
-  set -l default_node_version (nvm version default)
-  set -l node_version (nvm version)
-  set -l nvmrc_path (nvm_find_nvmrc)
-end
--- fish/functions/nvm_find_nvmrc.fish
+++
@@ -1,3 +0,0 @@
-function nvm_find_nvmrc
-  bass source ~/.nvm/nvm.sh --no-use
-end
